17a29bd87eafdab4df2f4bd1b52bf591e1149eaa
nguyenhoan <hoanpro24092002@gmail.com>
2021-09-27 16:21:02 +0700

    second change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
      * @param args arguments.
      */
     public static void main(String[] args) {
-        System.out.println("Hello Demo");
+        System.out.println("Hello Hoan");
     }
 }
