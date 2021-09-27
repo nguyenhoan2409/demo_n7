f39b3ff29476dd87284735169489e082590a349a
nguyenhoan <hoanpro24092002@gmail.com>
2021-09-27 16:57:42 +0700

    20020407-NguyenDangManhHoan

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
      * @param args arguments.
      */
     public static void main(String[] args) {
-        System.out.println("Hello Hoan, chuc mot ngay tot lanh");
+        System.out.println("Hello Hoan, HelloDemo");
     }
 }
